4d5f7bec1e27886faf6216f06506fde943e28af4
GaneSh1927 <154739991+GaneSh1927@users.noreply.github.com>
2023-12-28 13:42:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I PROMISE YOU ILL ALWAYS KEEP YOU HAPPY HONEYY<br>I WILL NEVER HURT YOU HONEYY NOT LETTING THOSE TEARS FALL FROM MY HONEYY EYES<br>I LOVEE YOUU SOO SOO MUCH HONEYY(INFINITE)<br>WISHING MAH HONEYY ATHAYA GARUU MAVAYA GARUU COOKIE A VERY HAPPY NEW YEAR";
+  gif.src =
+    "https://i.pinimg.com/originals/f7/f2/34/f7f234f2fff0b7c55a293b168571975b.gif";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
